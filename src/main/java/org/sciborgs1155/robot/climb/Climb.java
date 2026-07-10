1644a93a3b9c2f86ba3716536ce42d4857b6548f
Tirtha <tirthas970@gmail.com>
2026-07-10 11:25:42 -0400

    workign on climb.java

--- src/main/java/org/sciborgs1155/robot/climb/Climb.java
+++ src/main/java/org/sciborgs1155/robot/climb/Climb.java
@@ -1,5 +1,23 @@
 package org.sciborgs1155.robot.climb;
 
-public class Climb {
+import org.sciborgs1155.robot.Robot;
+
+
+public class Climb implements AutoCloseable {
+    private final ClimbIO hardware;
+
+    public static Climb create() {
+        return Robot.isReal() ? new Climb(new RealClimb()) : new Climb(new SimClimb());
+    }
+
+    public Climb(ClimbIO hardware) {
+        this.hardware = hardware;
+    }
+
+
+
+    }
+
+
     
-}
+
