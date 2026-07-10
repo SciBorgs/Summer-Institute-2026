ea6a6794c7801124be36f818b7e0fd040399a67d
Tirtha <tirthas970@gmail.com>
2026-07-10 10:03:34 -0400

    finnished real climb

--- src/main/java/org/sciborgs1155/robot/climb/ClimbConstants.java
+++ src/main/java/org/sciborgs1155/robot/climb/ClimbConstants.java
@@ -1,5 +1,52 @@
 package org.sciborgs1155.robot.climb;
 
-public class ClimbConstants {
-    
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Pounds;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearAcceleration;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Mass;
+
+//copied from rebuilt 
+public final class ClimbConstants {
+
+  public static final double P = 100;
+  public static final double I = 0;
+  public static final double D = 0;
+
+  public static final LinearAcceleration SPRING_ACCELERATION =
+      MetersPerSecondPerSecond.of(20); 
+
+  public static final double S = 0;
+  public static final double G = 0;
+  public static final double V = 0;
+  public static final double A = 0;
+
+  public static final LinearVelocity MAX_VELOCITY = MetersPerSecond.of(.5);
+  public static final LinearAcceleration MAX_ACCEL = MetersPerSecondPerSecond.of(.2);
+
+  public static final double GEARING = 60.0; 
+  public static final Distance WINCH_DIAMETER = Inches.of(0.750);
+  public static final double SENSOR_TO_MECHANISM_RATIO =
+      WINCH_DIAMETER.in(Meters)
+          * Math.PI
+          / GEARING; //changed it to meters (but it may be wrong)
+  public static final Current STATOR_LIMIT = Amps.of(60);
+  public static final Current SUPPLY_LIMIT = Amps.of(100);
+  public static final Distance POSITION_TOLERANCE = Inches.of(0.5);
+
+  public static final DCMotor GEARBOX = DCMotor.getKrakenX60(2);
+  public static final double MOI = 0.001;
+  public static final Distance MIN_HEIGHT = Inches.of(18.032);
+  public static final Distance MAX_HEIGHT = Inches.of(30);
+  public static final Distance STARTING_HEIGHT = MAX_HEIGHT;
+  public static final Mass WEIGHT = Pounds.of(6.42); 
+  public static final Distance SPROCKET_RADIUS = Inches.of(5);
 }
--- src/main/java/org/sciborgs1155/robot/climb/RealClimb.java
+++ src/main/java/org/sciborgs1155/robot/climb/RealClimb.java
@@ -1,5 +1,70 @@
 package org.sciborgs1155.robot.climb;
 
-public class RealClimb {
-    
+
+import static edu.wpi.first.units.Units.Amps;
+
+import org.sciborgs1155.lib.FaultLogger;
+import org.sciborgs1155.lib.TalonUtils;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+public class RealClimb implements ClimbIO{
+
+    private final TalonFX rightMotor;
+    private final TalonFX leftMotor; 
+
+    /* Configs & Initialization */
+    public RealClimb() {
+        rightMotor = new TalonFX(33); //for now 
+        leftMotor = new TalonFX(34); //for now
+
+        TalonFXConfiguration configs = new TalonFXConfiguration();
+
+        configs.MotorOutput.NeutralMode = NeutralModeValue.Brake; // stops the motor
+        configs.MotorOutput.Inverted =
+            InvertedValue.CounterClockwise_Positive; // counterclockwise is postive
+        configs.CurrentLimits.StatorCurrentLimit = ClimbConstants.STATOR_LIMIT.in(Amps);
+        configs.CurrentLimits.SupplyCurrentLimit = ClimbConstants.SUPPLY_LIMIT.in(Amps);
+        configs.Feedback.SensorToMechanismRatio = ClimbConstants.SENSOR_TO_MECHANISM_RATIO; 
+
+        rightMotor.setControl(new Follower(34, MotorAlignmentValue.Aligned));
+
+        rightMotor.getConfigurator().apply(configs);
+        leftMotor.getConfigurator().apply(configs);
+
+        FaultLogger.register(rightMotor);
+        FaultLogger.register(leftMotor);
+
+        TalonUtils.addMotor(rightMotor);
+        TalonUtils.addMotor(leftMotor);
+    }
+
+    @Override
+    public void setVoltage(double voltage) {
+        rightMotor.setVoltage(voltage);
+
+    }
+
+    @Override
+    public double getPosition() {
+        return rightMotor.getPosition().getValueAsDouble() * ClimbConstants.SENSOR_TO_MECHANISM_RATIO;
+    }
+
+    @Override
+    public double velocity() {
+        return leftMotor.getVelocity().getValueAsDouble() * ClimbConstants.SENSOR_TO_MECHANISM_RATIO;
+    }
+
+    @Override
+    public void close() throws Exception {
+        rightMotor.close();
+    }
+
+
 }
+    
