9231055996d8bd39a252b8be6068e6e2197273d0
Tirtha <tirthas970@gmail.com>
2026-07-10 11:00:36 -0400

    deleted brake config

--- src/main/java/org/sciborgs1155/robot/shooter/RealWheel.java
+++ src/main/java/org/sciborgs1155/robot/shooter/RealWheel.java
@@ -27,7 +27,6 @@ public class RealWheel implements WheelIO {
 
     TalonFXConfiguration configs = new TalonFXConfiguration();
 
-    configs.MotorOutput.NeutralMode = NeutralModeValue.Brake; // stops the motor
     configs.MotorOutput.Inverted =
         InvertedValue.CounterClockwise_Positive; // counterclockwise is postive
     configs.CurrentLimits.StatorCurrentLimit = STATOR_CURRENT_LIMIT.in(Amps);
