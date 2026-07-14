fb6a55a9cd0f50b87036a5369965b0253cc61d78
Tirtha <tirthas970@gmail.com>
2026-07-14 09:20:54 -0400

    finished visualizer i think

--- src/main/java/org/sciborgs1155/robot/climb/ClimbVisualizer.java
+++ src/main/java/org/sciborgs1155/robot/climb/ClimbVisualizer.java
@@ -2,16 +2,16 @@ package org.sciborgs1155.robot.climb;
 
 import static edu.wpi.first.units.Units.Meters;
 
-import com.reduxrobotics.sensors.canandcolor.ColorFrame;
-
 import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 
 public class ClimbVisualizer {
     //put not logged
-    private final Mechanism2d mech; // The physical field the robot is in
+    @NotLogged private final Mechanism2d mech; // The physical field the robot is in
     private final String name;
     private final MechanismLigament2d climb; // The elevator mechanism
 
@@ -20,9 +20,22 @@ public class ClimbVisualizer {
     public ClimbVisualizer(String name, Color8Bit color) {
         this.name = name;
 
-        mech = new Mechanism2d(50, 50);
-        climb = new MechanismLigament2d("climb", ClimbConstants.MAX_HEIGHT.in(Meters) * 10, 90, 3, color); //multiply by 10 to scale
+        mech = new Mechanism2d(50, 50); //50 by 50 field 
 
+        MechanismRoot2d root = mech.getRoot("chasis", 2, 0); 
+        climb = root.append(
+            new MechanismLigament2d(
+                "climb", 
+                ClimbConstants.MAX_HEIGHT.in(Meters) * 10, //multiply by 10 to scale
+                90, 
+                3, 
+                color)); 
+
+    }
+
+    public void setLength(double length) {
+        climb.setLength(length * 10);
+        SmartDashboard.putData("Robot/climb/" + name, mech);
     }
 
 }
